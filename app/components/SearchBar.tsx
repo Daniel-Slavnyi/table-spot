86fa9deaf224ea003dbe53d3619a456a6ba859b0
Daniel-Slavnyi <dslavnyj00@gmail.com>
2023-04-03 22:35:45 +0300

    feat pages:  home/reserve,restaurant/details/menu,search. Create components of APP

---
+++ app/components/SearchBar.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import React from "react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+export default function SearchBar() {
+  const router = useRouter();
+  const [searchValue, setSearchValue] = useState("");
+
+  return (
+    <div className="text-left text-lg py-3 m-auto flex justify-center">
+      <input
+        className="rounded  mr-3 p-2 w-[450px]"
+        type="text"
+        placeholder="State, city or town"
+        value={searchValue}
+        onChange={(e) => {
+          setSearchValue(e.target.value);
+        }}
+      />
+      <button
+        className="rounded bg-red-600 px-9 py-2 text-white"
+        onClick={() => {
+          if (searchValue === "banana" || searchValue === "") return;
+          router.push("/search");
+        }}
+      >
+        Lets go
+      </button>
+    </div>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+    fontSize: {
+      sm: "12px",
+    },
+  },
+  plugins: [],
+};
